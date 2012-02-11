48723011bc5d726240ed5b16d005ac973fc6cfad
Andrew Shewring <a.shewring@gmail.com>
2012-02-11 13:49:20 +0100

    Using a template-language agnostic way to invoke model-to-text transformations

--- org.s23m.cell.eclipse.xtend2/src/org/s23m/cell/eclipse/xtend/TestHtmlGeneration.java
+++ org.s23m.cell.eclipse.xtend2/src/org/s23m/cell/eclipse/xtend/TestHtmlGeneration.java
@@ -1,6 +1,10 @@
 package org.s23m.cell.eclipse.xtend;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.s23m.cell.Set;
+import org.s23m.cell.generator.ModelToTextTransformation;
 import org.s23m.cell.kernel.artifactinstantiation.InstantiationSequences;
 import org.s23m.cell.kernel.artifactinstantiation.RunInstantiationSequence;
 
@@ -11,7 +15,18 @@ public class TestHtmlGeneration {
 		RunInstantiationSequence.run();
 		// 
 		Set set = InstantiationSequences.getInstance().crm;
-		HtmlTemplate template = new HtmlTemplate();
-		System.out.println("Executing template:\n" + template.main(set));
+		
+		ModelToTextTransformation template = new ModelToTextTransformation() {
+			public CharSequence invoke(List<Set> parameters) {
+				if (parameters.size() != 1) {
+					throw new IllegalArgumentException("Illegal number of parameters - expected 1");
+				}
+				final Set set = parameters.get(0);
+				HtmlTemplate html = new HtmlTemplate();
+				return html.main(set);
+			}
+		};
+		
+		System.out.println("Executing template:\n" + template.invoke(Arrays.asList(set)));
 	}
 }
