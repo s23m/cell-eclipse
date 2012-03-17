cb318e37a66943dc42d7a1843fe58b5cef7d6178
Andrew Shewring <a.shewring@gmail.com>
2012-03-17 13:13:34 +0100

    Made the script more flexible

--- mvn-clean-build.sh
+++ mvn-clean-build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-if [ -n "${M2_HOME+x}" ] && [ -d $M2_HOME ]
+export WHICH_MVN=`which mvn`
+if [ -n "${WHICH_MVN}" ]
+then
+	export MVN_EXECUTABLE="$WHICH_MVN"
+	echo "mvn command is on the path - using '$MVN_EXECUTABLE' to run script"
+elif [ -n "${M2_HOME+x}" ] && [ -d $M2_HOME ]
+then
+	export MVN_EXECUTABLE="$M2_HOME/bin/mvn"
+	echo "M2_HOME is a valid directory - using '$MVN_EXECUTABLE' to run script"
+else
+	echo "Maven command (mvn) must be on the PATH or Maven home must be set as the M2_HOME environment variable"
+fi
+
+echo "Resolved mvn location: $MVN_EXECUTABLE"
+if [ -n "${MVN_EXECUTABLE+x}" ]
 then
 	# Remove Tycho p2 caches
 	rm -rf ~/.m2/repository/.cache
@@ -11,7 +25,5 @@ then
 
 	# Execute build
 	set MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=256m"
-	bash $M2_HOME/bin/mvn clean install
-else 
-	echo "Maven home must be set as M2_HOME environment variable"
+	bash $MVN_EXECUTABLE clean install
 fi
